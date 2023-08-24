9653398b9fc33033cccb60aa9b6e4d1074dd17c5
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-24 13:56:25 -0600

    set up gems

---
+++ app/controllers/villagers_controller.rb
@@ -0,0 +1,13 @@
+class VillagersController < ApplicationController
+  def index 
+    @villagers = ["Villager 1", "Villager 2", "Villager 3"]
+  end
+
+  def new
+
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/", to: "welcome#index"
+  get "/villagers", to: "villagers#index"
+  get "/villagers/new", to: "villagers#new"
+  post "/villagers", to: "villager#create"
 end
