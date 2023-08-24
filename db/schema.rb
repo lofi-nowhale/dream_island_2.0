3a66c4a29aa4c99abf6cb3ce35d374442a2252b5
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-24 16:04:00 -0600

    schema non associated yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_214451) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_215309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "islands", force: :cascade do |t|
+    t.string "name"
+    t.integer "rank"
+    t.boolean "full"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "villagers", force: :cascade do |t|
     t.string "name"
