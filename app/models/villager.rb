4fd5aa4790c9d67ece732e1be535f80d0aa9a9b0
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-26 16:14:12 -0600

    feat: add relationship associate and validations to the island model and the villager model

--- app/models/villager.rb
+++ app/models/villager.rb
@@ -1,3 +1,8 @@
 class Villager < ApplicationRecord
   belongs_to :island
+
+  validates_presence_of :name
+  validates_presence_of :personality
+  validates_presence_of :friendship_level
+  validates :dream_home, inclusion: [true,false]
 end
