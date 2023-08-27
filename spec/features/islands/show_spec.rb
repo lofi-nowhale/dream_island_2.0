1f8b2b450ee24e562bbd0f434114f01e7b4a3213
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-27 16:22:59 -0600

    feat: complete user story 2

--- spec/features/islands/show_spec.rb
+++ spec/features/islands/show_spec.rb
@@ -6,10 +6,43 @@ RSpec.describe 'the Island Show page' do
   end
 
   it 'shows the Island with the given ID and the attributes for that Island' do 
-    visit '/islands/:id'
+    visit "/islands/#{@duck_pond.id}"
+    save_and_open_page
     
-    expect(page).to have_content(island.name)
-    expect(page).to have_content(island.rank)
-    expect(page).to have_content(island.full)
+    expect(page).to have_content(@duck_pond.name)
+    expect(page).to have_content(@duck_pond.rank)
+    expect(page).to have_content(@duck_pond.full)
+    
+    visit "/islands/#{@bear_den.id}"
+
+    expect(page).to have_content(@bear_den.name)
+    expect(page).to have_content(@bear_den.rank)
+    expect(page).to have_content(@bear_den.full)
+
+    visit "/islands/#{@flamingo_flamboyance.id}"
+
+    expect(page).to have_content(@flamingo_flamboyance.name)
+    expect(page).to have_content(@flamingo_flamboyance.rank)
+    expect(page).to have_content(@flamingo_flamboyance.full)
+
+    visit "/islands/#{@koala_colony.id}"
+
+    expect(page).to have_content(@koala_colony.name)
+    expect(page).to have_content(@koala_colony.rank)
+    expect(page).to have_content(@koala_colony.full)
+
+    visit "/islands/#{@dog_park.id}"
+
+    expect(page).to have_content(@dog_park.name)
+    expect(page).to have_content(@dog_park.rank)
+    expect(page).to have_content(@dog_park.full)
+
+    visit "/islands/#{@cat_cafe.id}"
+
+    expect(page).to have_content(@cat_cafe.name)
+    expect(page).to have_content(@cat_cafe.rank)
+    expect(page).to have_content(@cat_cafe.full)
+
+    
   end
 end
