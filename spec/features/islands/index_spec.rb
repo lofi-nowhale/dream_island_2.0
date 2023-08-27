de66a2353dc0aa5686d43174a22a1e57cbbd43c8
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-27 12:05:18 -0600

    fix: test_helper was missing a value for the a villagers friendship level which was causing it to error out when running db:seed and when trying to load the file for the feature tests

--- spec/features/islands/index_spec.rb
+++ spec/features/islands/index_spec.rb
@@ -4,13 +4,19 @@ require 'rails_helper'
 #When I visit '/parents'
 #Then I see the name of each parent record in the system
 
-RSpec.describe 'the Island show page' do 
+RSpec.describe 'the Island show page' do
+  before(:each) do
+    load_test_data
+  end
+
   it 'shows all of the islands' do 
     visit "/islands"
 
-    
-
-    
-    
+    expect(page).to have_content(@duck_pond.name)
+    expect(page).to have_content(@bear_den.name)
+    expect(page).to have_content(@flamingo_flamboyance.name)
+    expect(page).to have_content(@koala_colony.name)
+    expect(page).to have_content(@dog_park.name)
+    expect(page).to have_content(@cat_cafe.name)
   end
 end
