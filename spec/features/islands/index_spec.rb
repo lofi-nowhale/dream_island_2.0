60071bc9274ada1772d33b61bf07cb977fb18997
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-26 21:02:28 -0600

    test: create test files for the first user story

---
+++ spec/features/islands/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+#For each parent table
+#As a visitor
+#When I visit '/parents'
+#Then I see the name of each parent record in the system
+
+RSpec.describe 'the Island show page' do 
+  it 'shows all of the islands' do 
+    visit "/islands"
+
+    
+
+    
+    
+  end
+end
