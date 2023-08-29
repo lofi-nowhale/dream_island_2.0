9c70047d45e970fa66124d830f811fd30eb0fb64
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-29 15:46:17 -0600

    test: write a test for the class method for the islands ordered by creation date

--- spec/models/island_spec.rb
+++ spec/models/island_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe Island, type: :model do
     it { should allow_value(true).for(:full) }
     it { should allow_value(false).for(:full) }
   end
+
+  describe "islands ordered by created - from most recent" do 
+    before (:each) do 
+      load_test_data
+    end
+    it "can return a list of islands ordered by their creation date and time by ascending order" do 
+       
+      expect(Island.sort_by_recently_created).to eq([@duck_pond, @bear_den, @flamingo_flamboyance, @koala_colony, @dog_park, @cat_cafe])
+    end
+  end
 end
